f6152dce3294d32eacc278cd6b24f382b84767cd
Dhruv Chheda <chhedadhruv1234@gmail.com>
2022-02-09 10:12:29 +0530

    update

--- navbar.js
+++ navbar.js
@@ -3,6 +3,3 @@ const navLinks = document.querySelector('.navLinks');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('hide');
 })
-function myFunc() {
-    onnavLinks.classList.toggle('hide'))
-}
